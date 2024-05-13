f154700bd6c140593fbcf8f75974fab010f0400c
Sakshi Antala <sakshi@saasinnova.com>
2024-05-13 10:48:46 +0530

    Remove unused react tag

--- VersionCheck.tsx
+++ VersionCheck.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Toast } from 'primereact/toast';
 import { useRef } from 'react';
 import { UpdateStatus } from './types';
